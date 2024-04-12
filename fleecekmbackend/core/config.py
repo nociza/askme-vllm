8fd41137e847d8d6e6a8aa4d13c8cb13694c55bc
nociza <azicon@berkeley.edu>
2024-04-12 15:50:10 -0700

    Refactor database configuration and add aiomysql and cryptography dependencies

--- fleecekmbackend/core/config.py
+++ fleecekmbackend/core/config.py
@@ -1,2 +1,2 @@
-DATABASE_URL = "sqlite+aiosqlite:///./db/dataset.db"
+DATABASE_URL = "mysql+aiomysql://root:fleecekm@localhost:13306/fleecekm"
 DATASET_PATH = "data/wiki_text_cleaned_v1.csv"
--- fleecekmbackend/db/models.py
+++ fleecekmbackend/db/models.py
@@ -5,12 +5,12 @@ from fleecekmbackend.db.ctl import Base
 class Paragraph(Base): # change to paragraph
     __tablename__ = "paragraph"
     id = Column(Integer, primary_key=True, index=True)
-    page_name = Column(String)
-    section_name = Column(String)
-    subsection_name = Column(String)
-    subsubsection_name = Column(String)
+    page_name = Column(String(1023))
+    section_name = Column(String(1023))
+    subsection_name = Column(String(1023))
+    subsubsection_name = Column(String(1023))
     text = Column(Text)
-    section_hierarchy = Column(String)
+    section_hierarchy = Column(String(8191))
     text_cleaned = Column(Text)
     word_count = Column(Integer) # use character count to estimate
     is_bad = Column(Boolean) # change to llm_quality_check or similar
@@ -21,19 +21,19 @@ class Paragraph(Base): # change to paragraph
 class Author(Base):
     __tablename__ = "author"
     id = Column(Integer, primary_key=True, index=True)
-    model = Column(String) # can be human
-    prompt = Column(String, nullable=True) 
-    username = Column(String, nullable=True)
+    model = Column(String(1023)) # can be human
+    prompt = Column(Text, nullable=True) 
+    username = Column(String(1023), nullable=True)
 
 class Question(Base):
     __tablename__ = "question"
     id = Column(Integer, primary_key=True, index=True)
     paragraph_id = Column(Integer) 
-    scope = Column(String) # the scope of the question, e.g. "single-paragraph"
+    scope = Column(String(1023)) # the scope of the question, e.g. "single-paragraph"
     context = Column(Text) # the context of the question for a fair zeroshot evaluation
     text = Column(Text)
     author_id = Column(Integer) 
-    timestamp = Column(String) 
+    timestamp = Column(String(255))
     upvote = Column(Integer)
     downvote = Column(Integer)
 
@@ -43,7 +43,7 @@ class Answer(Base):
     question_id = Column(Integer)
     author_id = Column(Integer)
     setting = Column(Enum("zs", "ic", "human"))
-    timestamp = Column(String)
+    timestamp = Column(String(255))
     text = Column(Text)
 
 class Rating(Base):
@@ -53,4 +53,4 @@ class Rating(Base):
     value = Column(Integer) # score from 1 to 5
     answer_id = Column(Integer)
     author_id = Column(Integer)
-    timestamp = Column(String)
+    timestamp = Column(String(255))

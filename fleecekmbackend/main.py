d076dc4448dc088dbba8d78138304d57e4b18379
nociza <azicon@berkeley.edu>
2024-04-13 00:17:33 -0700

    use lifespan instead

--- fleecekmbackend/main.py
+++ fleecekmbackend/main.py
@@ -2,6 +2,7 @@ import logging
 import asyncio
 
 from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
 
 from fleecekmbackend.api.dataset.raw import router as raw_dataset_router
 from fleecekmbackend.api.dataset.qa import router as qa_dataset_router
@@ -12,23 +13,12 @@ from fleecekmbackend.services.dataset.async_generate_qa import start_background_
 
 background_process_started = False
 
-app = FastAPI()
-
-# Include sub-routers
-app.include_router(raw_dataset_router, prefix="/raw", tags=["raw"])
-app.include_router(qa_dataset_router, prefix="/qa", tags=["qa"])
-
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to the WikiText API!"}
-
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await create_tables_if_not_exist()
     try:
         with open(DATASET_PATH, "r") as file:
             await load_csv_data(file)
-        pass
     except FileNotFoundError:
         logging.error("CSV file not found. Skipping data loading.")
     except Exception as e:
@@ -38,3 +28,14 @@ async def startup_event():
         if not background_process_started:
             asyncio.create_task(start_background_process())
             background_process_started = True
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+# Include sub-routers
+app.include_router(raw_dataset_router, prefix="/raw", tags=["raw"])
+app.include_router(qa_dataset_router, prefix="/qa", tags=["qa"])
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the WikiText API!"}

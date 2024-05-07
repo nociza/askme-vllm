b5aca83702462227209721235b98041b7d9528db
nociza <azicon@berkeley.edu>
2024-05-07 16:57:46 -0700

    Refactor eval_openai.py to use tqdm for progress tracking and logging for debugging

--- scripts/eval_openai.py
+++ scripts/eval_openai.py
@@ -3,12 +3,13 @@ from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.future import select
-from dotenv import dotenv_values
+from tqdm import tqdm
+import logging
 from fleecekmbackend.services.dataset.fleece_qa import (
     generate_answer,
     generate_answer_rating,
 )
-from fleecekmbackend.db.models import Question, Answer, Paragraph
+from fleecekmbackend.db.models import Question
 from fleecekmbackend.core.config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL)
@@ -22,7 +23,7 @@ async def process_questions():
         results = await db.execute(select(Question).order_by(func.random()).limit(50))
         questions = results.scalars().all()
 
-        for question in questions:
+        for question in tqdm(questions):
             answer_id_ic = await generate_answer(
                 db, question.id, "ic", model=MODEL, service="openai"
             )
@@ -30,20 +31,20 @@ async def process_questions():
                 db, question.id, "zs", model=MODEL, service="openai"
             )
 
-            print(
-                "Zero-shot answer: ",
+            logging.debug(
+                "Zero-shot answer ID: ",
                 answer_id_ic,
                 "\n",
-                "In-context answer: ",
+                "In-context answer ID: ",
                 answer_id_zs,
             )
 
             if answer_id_ic:
                 result_ic = await generate_answer_rating(db, question.id, answer_id_ic)
-                print("In-context Rating: ", result_ic)
+                logging.debug("In-context Rating ID: ", result_ic)
             if answer_id_zs:
                 result_zs = await generate_answer_rating(db, question.id, answer_id_zs)
-                print("Zeroshot Rating: ", result_zs)
+                logging.debug("Zeroshot Rating ID: ", result_zs)
 
 
 async def main():

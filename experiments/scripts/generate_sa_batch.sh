cfff4502017dfc40589f4538163da1465c6ae0db
nociza <azicon@berkeley.edu>
2024-06-12 19:54:11 -0700

    Refactor code to combine multiple CSV files into one

--- experiments/scripts/generate_sa_batch.sh
+++ experiments/scripts/generate_sa_batch.sh
@@ -2,9 +2,11 @@
 
 DATA_FILE="experiments/data_samples/askme-10k.csv"
 OUTPUT_DIR="experiments/data_samples/short_answers"
+OUTPUT_FILE="experiments/data_samples/short-answers-10k.csv"
 TEMP_DIR="temp_batches"
 LOGS_DIR="logs"
 SPLIT_SCRIPT="experiments/scripts/split_csv.py"
+COMBINE_SCRIPT="experiments/scripts/combine_csvs.py"
 
 NUM_INSTANCES=32
 
@@ -36,6 +38,12 @@ done
 
 wait
 
+python $COMBINE_SCRIPT $OUTPUT_DIR $OUTPUT_FILE
+
 rm -rf $TEMP_DIR
+
+rm -rf $LOGS_DIR
+
+rm -rf $OUTPUT_DIR
 
 echo "Processing completed. Results are in $OUTPUT_DIR and logs are in $LOGS_DIR."
